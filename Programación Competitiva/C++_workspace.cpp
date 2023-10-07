27b806ddf6e3056a3a20ae081f9e74104ec13544
Vanessa Reteguín <145515264+vcardui@users.noreply.github.com>
2023-10-06 18:39:59 -0600

    Create C++_workspace.cpp

---
+++ Programación Competitiva/C++_workspace.cpp
@@ -0,0 +1,14 @@
+// Vanessa Reteguín aka Cardui
+//link y/o nombre del programa
+
+#include<bits/stdc++.h>
+using namespace std;
+#define SPEED ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main () {
+  SPEED;
+
+  cout<<"Cardui";
+
+  return 0;
+}
